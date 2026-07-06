94446aece1270820b6b80dcf48e1d74ff67de5d9
Daniel Martinez Maimone <96289800+ZapatoProgramming@users.noreply.github.com>
2026-07-06 13:31:05 -0600

    Medianamente funcional

---
+++ MaiTODO/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MaiTODO
+//
+//  Created by Daniel Martínez Maimone on 06/07/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
