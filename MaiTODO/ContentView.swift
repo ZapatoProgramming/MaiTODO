cecb4bb5424445002dc839a9d380dc6f33ec3049
Daniel Martinez Maimone <96289800+ZapatoProgramming@users.noreply.github.com>
2026-07-06 10:41:00 -0600

    Initial Commit

---
+++ MaiTODO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MaiTODO
+//
+//  Created by Daniel Martínez Maimone on 06/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaiTODO/MaiTODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaiTODOApp.swift
+//  MaiTODO
+//
+//  Created by Daniel Martínez Maimone on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MaiTODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
